a07e5226cc1841f95fbf8700c18c498ac323b091
ASUS <ASUS@DESKTOP-2QU4MHV>
2017-03-04 12:59:04 +0700

    Table

---
+++ src/util/Sutil.java
@@ -0,0 +1,30 @@
+package util;
+
+
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
+public class Sutil {
+
+    public static void msg(Component parent, String message) {
+        JOptionPane.showMessageDialog(parent,
+                message,
+                "App Info",
+                1);
+    }
+    
+    public static void msgE(Component parent, String message) {
+        JOptionPane.showMessageDialog(parent,
+                message,
+                "App Info",
+                2);
+    }
+    
+    public static int msgQ(Component parent, String message) {
+        int respon = JOptionPane.showOptionDialog(parent,
+                message,
+                "App Info",
+                JOptionPane.YES_NO_OPTION, 3, null, null, null);
+        return respon;
+    }
+}
